3a54fb859ddbe0176df256f0d712d14a582790b6
Emilio Núñez <emiliojes@gmail.com>
2016-03-16 23:27:59 -0500

    Dibujando Circulos usando PI

--- canvas.js
+++ canvas.js
@@ -8,10 +8,17 @@ function inicio()
 	lineas.lineTo(300,300);
 	lineas.lineTo(0,300);
 	lineas.lineTo(0,0);
+	lineas.strokeStyle = "#FF00FF";
 	lineas.stroke();
 
-	lineas.moveTo(100,100);
-	lineas.lineTo(200,100);
+/*
+	Trazo nuevo para el circulo.
+*/
+
+	lineas.beginPath();
 	lineas.strokeStyle = "#FF00FF";
+	/*Parametros de arc = */
+	lineas.arc(150,150,100, (Math.PI * 2), false);
+	lineas.closePath();
 	lineas.stroke();
 }
